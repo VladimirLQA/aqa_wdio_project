cc592947d318a7ed840aa00018faabd4c01d4a0a
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2023-09-17 21:51:49 +0300

    ...

--- src/tests/dropdown.test.ts
+++ src/tests/dropdown.test.ts
@@ -15,31 +15,29 @@ describe("Dropdown test", () => {
 
     it("Should be displayed default option", async () => {
         await $(MainPageHerokuapp["Dropdown page link"]).click();
-        await $(DropdownPage["Dropdown page title"]).waitForDisplayed({
+        await DropdownPage["Dropdown page title"].waitForDisplayed({
             timeout: 5000,
             timeoutMsg: "Dropdown page not opened after 5 seconds",
             reverse: false
         });
 
-        await $(DropdownPage["Dropdown"]).isDisplayed();
-        const defaultOption = await $(DropdownPage["Dropdown selected option"]).getText();
+        expect(await DropdownPage["Dropdown"].isDisplayed()).toBeTruthy();
+        const defaultOption = await DropdownPage["Dropdown selected option"].getText();
         expect(defaultOption).toBe("Please select an option")
     });
 
     it("Should select 'option 1'", async () => {
-        await $(DropdownPage["Dropdown"]).click();
-        await $(DropdownPage["Dropdown Option 1"]).isDisplayed();
-        await $(DropdownPage["Dropdown Option 1"]).isClickable();
-        await $(DropdownPage["Dropdown Option 1"]).click();
-        const selectedOption = await $(DropdownPage["Dropdown selected option"]).getText();
+        await DropdownPage["Dropdown"].click();
+        expect(await DropdownPage["Dropdown Option 1"].isDisplayed()).toBeTruthy();
+        await DropdownPage["Dropdown Option 1"].click();
+        const selectedOption = await DropdownPage["Dropdown selected option"].getText();
         expect(selectedOption).toBe("Option 1");
     });
     it("Should select 'option 2'", async () => {
-        await $(DropdownPage["Dropdown"]).click();
-        await $(DropdownPage["Dropdown Option 2"]).isDisplayed();
-        await $(DropdownPage["Dropdown Option 2"]).isClickable();
-        await $(DropdownPage["Dropdown Option 2"]).click();
-        const selectedOption = await $(DropdownPage["Dropdown selected option"]).getText();
+        await DropdownPage["Dropdown"].click();
+        expect(await DropdownPage["Dropdown Option 2"].isDisplayed()).toBeTruthy();
+        await DropdownPage["Dropdown Option 2"].click();
+        const selectedOption = await DropdownPage["Dropdown selected option"].getText();
         expect(selectedOption).toBe("Option 2");
     });
 });
