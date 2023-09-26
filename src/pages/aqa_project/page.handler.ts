e3b8dccc3df7cbbc95e34c33cea1fdf0445034e9
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2023-09-26 23:30:27 +0300

    ...

--- src/pages/aqa_project/page.handler.ts
+++ src/pages/aqa_project/page.handler.ts
@@ -2,6 +2,7 @@ import {TIMEOUT_5} from "../../utils/aqa_project_const";
 
 export class PageHandler {
 
+    // TODO: implement basic logic to carry out an action on pages
     public async findElement(selector: string): Promise<WebdriverIO.Element> {
         try {
             const element = await $(selector);
