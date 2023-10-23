2a9185639c5c51d603bc07a9fb29d9760ab46a80
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2023-10-23 21:49:41 +0300

    ui types

--- src/ui/types/common.types.ts
+++ src/ui/types/common.types.ts
@@ -1,4 +1,14 @@
+import { MANUFACTURERS } from '../../data/products/product.data';
+import { ORDER_STATUS } from '../../data/orders/orders.data';
+import { COUNTRIES } from '../../data/customers/customers.data';
+
 type ActionButtons = 'Delete' | 'Details' | 'Edit';
 
+type UnionFilterModalLabels = MANUFACTURERS | ORDER_STATUS | COUNTRIES;
 
-export { ActionButtons };
+interface IChipsFilterOptions {
+  search?: string;
+  quickFilters?: string[];
+}
+
+export { ActionButtons, UnionFilterModalLabels, IChipsFilterOptions };
