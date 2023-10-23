663bf0874a2bb5b1ec70f73b1f3642958caf0363
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2023-10-23 21:47:53 +0300

    mapper data

---
+++ src/utils/mapper-keys.ts
@@ -0,0 +1,29 @@
+const apiKeysMapper = {
+  "_id": 'Order Number',
+  "email": "Email",
+  "name": "Name",
+  "country": "Country",
+  "city": "City",
+  "address": "Address",
+  "house": "House",
+  "flat": "Flat",
+  "street": "Street",
+  "phone": "Phone",
+  "createdOn": "Created On",
+  "notes": "Notes",
+  "manufacturer": "Manufacturer",
+  "price": "Price",
+  "amount": "Amount",
+  "status": "Order Status",
+  "delivery": "Delivery",
+  "condition": "Delivery Type",
+  "finalDate": "Delivery Date",
+  "total_price": "Total Price",
+  "customer": "Customer",
+};
+
+const tableHeadersForMapping = {
+  products: ['name', 'price', 'manufacturer'],
+}
+
+export { apiKeysMapper, tableHeadersForMapping }
