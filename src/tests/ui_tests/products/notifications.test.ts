51b6a75f924bbd2cb1c34e42ddc2911d1850ce5b
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2023-11-04 23:43:57 +0200

    some

---
+++ src/tests/ui_tests/products/notifications.test.ts
@@ -0,0 +1,52 @@
+import SignInActions from '../../../ui/actions/sign-in.actions';
+import HomeActions from '../../../ui/actions/home.actions';
+import ProductsActions from '../../../ui/actions/products/products.actions';
+import AddNewProductActions from '../../../ui/actions/products/add-new-product.actions';
+import ProductsAssertions from '../../../ui/assertions/products_assertions/products.assertions';
+import { productData } from '../../../data/products/products-test.data';
+import { getNewProduct } from '../../../data/products/product.data';
+import { ProductsStorage } from '../../../utils/storages/products.storage';
+import { reqAsLoggedUser } from '../../../api/request/request-as-logged-user';
+import ProductsController from '../../../api/controllers/products.controller';
+import { IProduct } from '../../../ui/types/products.types';
+
+describe('Notifications test on products page', () => {
+  let productToCreate: IProduct;
+  before('Up browser', async () => {
+    await SignInActions.openSalesPortal();
+  });
+
+  beforeEach('Prepare to test', async () => {
+    await SignInActions.signIn();
+    await HomeActions.openProductsPage();
+    await ProductsActions.openAddNewProductPage();
+  });
+
+  afterEach('Clear state after test', async () => {
+    await HomeActions.clickOnSignOutButton();
+  });
+
+  after('Tear down after test suite', async () => {
+    await SignInActions.signIn();
+    await HomeActions.openProductsPage();
+
+    for (const product of ProductsStorage.getAllProducts()) {
+      await ProductsActions.deleteProduct(product.name);
+    }
+  });
+
+  context('Positive tests on input fields validation', async () => {
+
+    for (const product of productData.valid.name) {
+      it(`Should create product with valid name: '${product.description}'`, async () => {
+        productToCreate = getNewProduct({ name: product.name });
+        await ProductsStorage.addProduct(productToCreate);
+
+        await AddNewProductActions.fillProductInputs(productToCreate);
+        await AddNewProductActions.clickOnSaveNewProductButton();
+        await ProductsAssertions.verifyProductToastText('deleted');
+      });
+    }
+  });
+
+});
