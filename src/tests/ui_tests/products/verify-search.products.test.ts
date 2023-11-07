6b1a81d0e95a0929060cffa597fdef4d9d39a8d6
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2023-11-07 23:31:56 +0200

    complete tests for search and quick filters

--- src/tests/ui_tests/products/verify-search.products.test.ts
+++ src/tests/ui_tests/products/verify-search.products.test.ts
@@ -1,14 +1,9 @@
 import SignInActions from '../../../ui/actions/sign-in.actions';
 import HomeActions from '../../../ui/actions/home.actions';
 import ProductsActions from '../../../ui/actions/products/products.actions';
-import { ProductsStorage } from '../../../utils/storages/products.storage';
-import ProductsController from '../../../api/controllers/products.controller';
-import { reqAsLoggedUser } from '../../../api/request/request-as-logged-user';
 import { MANUFACTURERS } from '../../../data/products/product.data';
 import ProductsPage from '../../../ui/pages/aqa_project/products/products.page';
 import ProductsAssertions from '../../../ui/assertions/products_assertions/products.assertions';
-import FilterModalPage from '../../../ui/pages/aqa_project/modals/filter-modal.page';
-import FiltersModalActions from '../../../ui/actions/modals/filters-modal.actions';
 
 describe('WDIO - 7', () => {
   before('Up browser and open products page', async () => {
@@ -17,28 +12,33 @@ describe('WDIO - 7', () => {
     await HomeActions.openProductsPage();
   });
 
+  afterEach('Clear chips', async () => {
+    await ProductsActions.clearQuickAndSearchFilterChips(ProductsPage, 'all');
+  })
+
   it('Should verify table data after search with filters', async () => {
-    await ProductsActions.clickOnFiltersButton();
-    await FiltersModalActions.checkFiltersBox(FilterModalPage, [MANUFACTURERS.APPLE, MANUFACTURERS.AMAZON, MANUFACTURERS.XIAOMI]);
-    await FiltersModalActions.clickOnApplyButton();
+
+    const quickFilters = [MANUFACTURERS.APPLE, MANUFACTURERS.AMAZON, MANUFACTURERS.XIAOMI];
+    await ProductsActions.checkQuickFilters(quickFilters)
+    const actualQuickFilters = (await ProductsActions.getListOfChipButtons(ProductsPage)).quickFilters;
+    expect(quickFilters.every((filter) => actualQuickFilters.includes(filter))).toBe(true);
     await ProductsAssertions.verifyTableData(ProductsPage);
 
   });
 
-
-  // todo add actions and finish tests
   it('Should verify table data after search with search value', async () => {
-    await ProductsActions.clickOnFiltersButton();
-    await FiltersModalActions.checkFiltersBox(FilterModalPage, [MANUFACTURERS.APPLE, MANUFACTURERS.AMAZON, MANUFACTURERS.XIAOMI]);
-    await FiltersModalActions.clickOnApplyButton();
+    await ProductsActions.fillSearchInput('app');
+    await ProductsActions.clickOnSearchButton();
+    await ProductsAssertions.verifyElementText(ProductsPage['Chip buttons'], 'app');
     await ProductsAssertions.verifyTableData(ProductsPage);
 
   });
 
   it('Should verify table data after search with search value and filters', async () => {
-    await ProductsActions.clickOnFiltersButton();
-    await FiltersModalActions.checkFiltersBox(FilterModalPage, [MANUFACTURERS.APPLE, MANUFACTURERS.AMAZON, MANUFACTURERS.XIAOMI]);
-    await FiltersModalActions.clickOnApplyButton();
+
+    await ProductsActions.checkQuickFilters([MANUFACTURERS.TESLA])
+    await ProductsActions.fillSearchInput('6');
+    await ProductsActions.clickOnSearchButton();
     await ProductsAssertions.verifyTableData(ProductsPage);
 
   });
