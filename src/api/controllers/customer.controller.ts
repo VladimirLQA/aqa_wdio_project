1185d4790feb0c67dfb3edede9988747be01ca08
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2023-11-11 14:41:55 +0200

    ...

--- src/api/controllers/customer.controller.ts
+++ src/api/controllers/customer.controller.ts
@@ -1,14 +1,14 @@
 import Request from '../request/request';
 import { Id, RequestOptions, RequestParams } from '../types/api-request.types';
-import { URLS } from '../endpoints/base-endpoints';
+import CustomerEndpoints from '../endpoints/customer-endpoints';
 import { IProduct } from '../../ui/types/products.types';
 
-class ProductsController {
+class ProductsController {CustomerEndpoints
   public async get(params: RequestParams<Id>) {
     const options: RequestOptions = {
       method: 'GET',
-      baseURL: URLS.baseURL,
-      url: params.data ? URLS.endpoints.productByID(params.data._id) : URLS.endpoints.products,
+      baseURL: CustomerEndpoints.baseURL,
+      url: params.data ? CustomerEndpoints.customerById(params.data._id) : CustomerEndpoints.customers,
       headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${params.token}` },
     };
     return Request.sendRequest(options);
@@ -17,8 +17,8 @@ class ProductsController {
   public async create(params: RequestParams<IProduct>) {
     const options: RequestOptions = {
       method: 'POST',
-      baseURL: URLS.baseURL,
-      url: URLS.endpoints.products,
+      baseURL: CustomerEndpoints.baseURL,
+      url: CustomerEndpoints.customers,
       headers: { Authorization: `Bearer ${params.token}` },
       data: params.data,
     };
@@ -28,8 +28,8 @@ class ProductsController {
   public async update(params: RequestParams<IProduct>) {
     const options: RequestOptions = {
       method: 'PUT',
-      baseURL: URLS.baseURL,
-      url: URLS.endpoints.products,
+      baseURL: CustomerEndpoints.baseURL,
+      url: CustomerEndpoints.customers,
       headers: { Authorization: `Bearer ${params.token}` },
       data: params.data,
     };
@@ -39,8 +39,8 @@ class ProductsController {
   public async delete(params: Required<RequestParams<Id>>) {
     const options: RequestOptions = {
       method: 'DELETE',
-      baseURL: URLS.baseURL,
-      url: URLS.endpoints.productByID(params.data._id),
+      baseURL: CustomerEndpoints.baseURL,
+      url: CustomerEndpoints.customerById(params.data._id),
       headers: { Authorization: `Bearer ${params.token}` },
     };
     return Request.sendRequest(options);
