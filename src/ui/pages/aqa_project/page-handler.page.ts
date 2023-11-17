051a6a577a649ee0d9afffb92e6efbcfa065dd0e
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2023-11-17 23:18:50 +0200

    page handler refactor

--- src/ui/pages/aqa_project/page-handler.page.ts
+++ src/ui/pages/aqa_project/page-handler.page.ts
@@ -4,32 +4,58 @@ import { browserPause } from '../../../utils/helpers';
 
 export default class PageHandler {
 
-  public async waitForElement(selector: string, timeout: number = TIMEOUT['5 seconds']): Promise<WebdriverIO.Element> {
+  async waitForElement(selector: string, reverse = false, timeout: number = TIMEOUT['5 seconds']): Promise<WebdriverIO.Element> {
     const elem = await elementFinder.findElement(selector);
-    await elem.waitForDisplayed({ timeout });
+    await elem.waitForDisplayed({
+      reverse,
+      timeout,
+      timeoutMsg: `Element with selector ${selector} was not found`,
+    });
     return elem;
   }
 
-  public async waitForElements(selector: string, timeout: number = TIMEOUT['5 seconds']): Promise<WebdriverIO.Element[]> {
+  async waitForElementsArrayToBeDisplayed(selector: string, reverse = false, timeout: number = TIMEOUT['5 seconds']): Promise<WebdriverIO.Element[]> {
     const elements = await elementFinder.findArrayElements(selector);
+    await browser.waitUntil(async () => {
+      for(const elem of elements) {
+        await elem.waitForDisplayed({
+          reverse,
+          timeout,
+        })
+      }
+      return true;
+    }, {timeout, timeoutMsg: `Error during waiting for array of elements with selector ${selector}`})
     return elements;
   }
 
-  public async waitForElemAndSetValue(selector: string, text: string | number, timeout: number = TIMEOUT['5 seconds']): Promise<void> {
-    const elem = await this.waitForElement(selector);
+  async waitForElementAndScroll(selector: string, timeout: number = TIMEOUT['5 seconds']) {
+    try {
+      const element = await this.waitForElement(selector);
+      await element.waitForExist({timeout});
+      await element.scrollIntoView({block: 'center'});
+      return element;
+
+    } catch (error) {
+        throw error;
+    }
+
+  }
+
+  async waitForElemAndSetValue(selector: string, text: string | number, timeout: number = TIMEOUT['5 seconds']): Promise<void> {
+    const elem = await this.waitForElementAndScroll(selector);
     await elem.waitForEnabled({ timeout, timeoutMsg: 'Element is not enabled after 5 seconds' });
-    await elem.setValue(text)
+    await elem.setValue(text);
     await browserPause(200);
   }
 
-  public async waitForElemAndClick(selector: string, timeout: number = TIMEOUT['5 seconds']): Promise<void> {
-    const elem = await this.waitForElement(selector);
+  async waitForElemAndClick(selector: string, timeout: number = TIMEOUT['5 seconds']): Promise<void> {
+    const elem = await this.waitForElementAndScroll(selector);
     await elem.waitForEnabled({ timeout, timeoutMsg: 'Element is not enabled after 5 seconds' });
     await elem.click();
   }
 
-  public async waitForElemAndGetText(selector: string): Promise<string> {
-    const elem = await this.waitForElement(selector);
+  async waitForElemAndGetText(selector: string): Promise<string> {
+    const elem = await this.waitForElementAndScroll(selector);
     const text = await elem.getText();
     return text;
   }
