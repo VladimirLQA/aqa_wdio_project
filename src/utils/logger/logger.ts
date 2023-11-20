8acb534a28bbd286cd549a07fa7e8520e09ff984
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2023-11-20 21:37:22 +0200

    logger added

--- src/utils/logger/logger.ts
+++ src/utils/logger/logger.ts
@@ -1,1 +1,53 @@
-// TODO implement logger
+import * as winston from "winston";
+import _ from "lodash";
+import { attachLog } from '../reporter/allure.reporter';
+
+
+type logLevels = "info" | "error";
+
+class Logger {
+  private logArray: string[] = [];
+  private static instance: Logger;
+  private logger = winston.createLogger({
+    level: "info",
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.timestamp(),
+      winston.format.printf(({ timestamp, level, message }) => `${timestamp} [${level}]: ${message}`)
+    ),
+    transports: [new winston.transports.Console()],
+  });
+
+  constructor() {
+    if (Logger.instance) {
+      return Logger.instance;
+    }
+    Logger.instance = this;
+  }
+
+  log(message: string, level: logLevels = "info") {
+    const logEntry = `${new Date().toISOString()} [${level.toUpperCase()}]: ${message}`;
+    this.logArray.push(logEntry);
+    this.logger.log({ level, message });
+  }
+
+  logApiRequest(requestInfo: string) {
+    this.log(`API Request: ${requestInfo}`);
+  }
+
+  logApiResponse(responseInfo: string, level: logLevels = "info") {
+    this.log(`API Response: ${responseInfo}`);
+  }
+
+  sendLogsToReport() {
+    const log = this.logArray.join("\n");
+    attachLog(log);
+    this.clearLogs();
+  }
+
+  clearLogs() {
+    _.remove(this.logArray);
+  }
+}
+
+export default new Logger();
