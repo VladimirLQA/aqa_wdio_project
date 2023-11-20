ac144645f8a26358c74ff549743721f4b9c81f00
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2023-11-20 21:37:58 +0200

    reporter logs

--- src/utils/reporter/allure.reporter.ts
+++ src/utils/reporter/allure.reporter.ts
@@ -1,5 +1,7 @@
 import allure from '@wdio/allure-reporter';
 import { Status } from 'allure-js-commons';
+import { AxiosRequestConfig, AxiosResponse } from 'axios';
+import Logger from '../logger/logger';
 
 export function logAction(stepName: string): MethodDecorator {
   return function(target: any, propertyKey: string | symbol, descriptor: PropertyDescriptor) {
@@ -25,4 +27,45 @@ export function logAction(stepName: string): MethodDecorator {
     };
     return descriptor;
   };
+}
+
+export function attachLog(log: string) {
+  allure.addAttachment('Test log', log, 'text/plain');
+}
+
+export function attachScreenshot(screenshot: string) {
+  allure.addAttachment('Screenshot', Buffer.from(screenshot, 'base64'), 'image/png');
+}
+
+export function logApiActions(target: any, propertyName: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+
+  descriptor.value = async function(...args: any[]) {
+    const options: AxiosRequestConfig = args[0];
+
+    allure.startStep(`Request: ${options.method.toUpperCase()} ${options.url}`);
+    allure.addAttachment(`Request headers`, JSON.stringify(options.headers, null, 2), 'application/json');
+    allure.addAttachment(`Request body`, JSON.stringify(options.data, null, 2), 'application/json');
+    allure.endStep();
+
+    Logger.logApiRequest(JSON.stringify(options));
+
+    try {
+      const response: AxiosResponse = await originalMethod.apply(this, args);
+
+      allure.startStep(`Response: ${response.status} ${response.config.url}`);
+      allure.addAttachment(`Response headers`, JSON.stringify(response.headers, null, 2), 'application/json');
+      allure.addAttachment(`Response body`, JSON.stringify(response.data, null, 2), 'application/json');
+      allure.endStep(response.status >= 400 ? Status.FAILED : Status.PASSED);
+
+      Logger.logApiResponse(JSON.stringify({status: response.status, body: response.data}));
+
+      return response;
+    } catch (error) {
+      allure.endStep(Status.FAILED);
+
+      throw error;
+    }
+  };
+  return descriptor;
 }
