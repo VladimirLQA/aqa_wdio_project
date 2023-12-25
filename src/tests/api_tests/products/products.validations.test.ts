7ae10c3d53847f7dbeeb186b499c30b5d50c3483
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2023-12-26 00:47:51 +0200

    api tests added

--- src/tests/api_tests/products/products.validations.test.ts
+++ src/tests/api_tests/products/products.validations.test.ts
@@ -4,7 +4,7 @@ import ApiProductsAssertions from '../../../api/api_assertions/api-products.asse
 import ProductsController from '../../../api/controllers/products.controller';
 import { reqAsLoggedUser } from '../../../api/request/request-as-logged-user';
 import { IProductResponse } from '../../../api/type/api.product.type';
-import { getNewProduct } from '../../../data/products/product.data';
+import { errorMessage, getNewProduct } from '../../../data/products/product.data';
 import { productData } from '../../../data/products/products-test.data';
 import { productsStorage } from '../../../utils/storages/storages';
 
@@ -31,49 +31,78 @@ describe('Smoke api test', () => {
     }
   });
 
-  context('Tests with valid data', () => {
-    for (const product of productData.valid.name) {
-      it(`Should create product with name: '${product.description}'`, async () => {
-        const response = await ApiProductsActions.createProduct(token, getNewProduct(product));
+  xcontext('Tests with valid data', () => {
+    for (const productName of productData.valid.name) {
+      it(`Should create product with name: '${productName.description}'`, async () => {
+        const response = await ApiProductsActions.createProduct(token, getNewProduct({ name: productName.name }));
+        console.log(response.data);
         await ApiProductsAssertions.verifyResponse(response, 201, true, null);
 
         productsStorage.addEntity(response.data.Product);
       });
     }
 
-    for (const product of productData.valid.price) {
-      it(`Should create product with price: '${product.description}'`, async () => {
-        const response = await ApiProductsActions.createProduct(token, getNewProduct(product));
+    for (const productPrice of productData.valid.price) {
+      it(`Should create product with price: '${productPrice.description}'`, async () => {
+        const response = await ApiProductsActions.createProduct(token, getNewProduct({ price: productPrice.price }));
         await ApiProductsAssertions.verifyResponse(response, 201, true, null);
 
         productsStorage.addEntity(response.data.Product);
       });
     }
 
-    for (const product of productData.valid.amount) {
-      it(`Should create product with amount: '${product.amount}'`, async () => {
-        const response = await ApiProductsActions.createProduct(token, getNewProduct(product));
+    for (const productAmount of productData.valid.amount) {
+      it(`Should create product with amount: '${productAmount.amount}'`, async () => {
+        const response = await ApiProductsActions.createProduct(token, getNewProduct({ amount: productAmount.amount }));
         await ApiProductsAssertions.verifyResponse(response, 201, true, null);
 
         productsStorage.addEntity(response.data.Product);
       });
     }
 
-    for (const product of productData.valid.manufacturer) {
-      it(`Should create product with manufacturer: '${product.manufacturer}'`, async () => {
-        const response = await ApiProductsActions.createProduct(token, getNewProduct(product));
+    for (const productManufacturer of productData.valid.manufacturer) {
+      it(`Should create product with manufacturer: '${productManufacturer.manufacturer}'`, async () => {
+        const response = await ApiProductsActions.createProduct(
+          token,
+          getNewProduct({ manufacturer: productManufacturer.manufacturer }),
+        );
         await ApiProductsAssertions.verifyResponse(response, 201, true, null);
 
         productsStorage.addEntity(response.data.Product);
       });
     }
 
-    for (const product of productData.valid.notes) {
-      it(`Should create product with notes: '${product.notes}'`, async () => {
-        const response = await ApiProductsActions.createProduct(token, getNewProduct(product));
+    for (const productNotes of productData.valid.notes) {
+      it(`Should create product with notes: '${productNotes.notes}'`, async () => {
+        const response = await ApiProductsActions.createProduct(token, getNewProduct({ notes: productNotes.notes }));
         await ApiProductsAssertions.verifyResponse(response, 201, true, null);
 
         productsStorage.addEntity(response.data.Product);
+      });
+    }
+  });
+
+  context('Tests with invalid data', () => {
+    for (const productName of productData.invalid.name) {
+      it(`Should not create product with name: ${productName.description}`, async () => {
+        const response = await ApiProductsActions.createProduct(token, getNewProduct({ name: productName.name }));
+        await ApiProductsAssertions.verifyResponse(response, 400, false, errorMessage['incorrect request body']());
+      });
+    }
+
+    it('Should not create product with existed name', async () => {
+      const product = getNewProduct();
+      const preparedProduct = await ApiProductsActions.createProduct(token, product);
+      const response = await ApiProductsActions.createProduct(token, product);
+      await ApiProductsAssertions.verifyResponse(response, 409, false, errorMessage['already exist'](product.name));
+
+      productsStorage.addEntity(preparedProduct.data.Product);
+    });
+
+    for (const productPrice of productData.invalid.price) {
+      it(`Should not create product with price: ${productPrice.description}`, async () => {
+        const response = await ApiProductsActions.createProduct(token, getNewProduct({ price: productPrice.price }));
+        await ApiProductsAssertions.verifyResponse(response, 400, false, errorMessage['incorrect request body']());
       });
     }
   });
