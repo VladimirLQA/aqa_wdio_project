8211ce744c1a7674bdde73792c7fec416fee7355
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2024-01-30 23:07:42 +0200

    add actions for orders page and order details page; test fix;

--- src/tests/ui_tests/orders/smoke.orders.test.ts
+++ src/tests/ui_tests/orders/smoke.orders.test.ts
@@ -1,3 +1,5 @@
+import { ICustomerResponse } from '../../../api/type/api.customers.type.js';
+import { IProductResponse } from '../../../api/type/api.product.type.js';
 import { orderPageToastMessages } from '../../../data/orders/orders.data.js';
 import HomeActions from '../../../ui/actions/home.actions.js';
 import CreateOrderModalActions from '../../../ui/actions/modals/orders_modals/create-order.modal.actions.js';
@@ -5,11 +7,22 @@ import OrderActions from '../../../ui/actions/orders/orders.actions.js';
 import SignInActions from '../../../ui/actions/sign-in.actions.js';
 import OrdersAssertions from '../../../ui/assertions/orders_assertions/orders.assertions.js';
 import CreateOrderModalPage from '../../../ui/pages/aqa_project/modals/orders_modals/create-order.modal.page.js';
+import ApiProductsActions from '../../../api/api_actions/api-products.actions.js';
+import ApiCustomersActions from '../../../api/api_actions/api-customers.actions.js';
+import OrdersDetailsActions from '../../../ui/actions/orders/orders-details.actions.js';
+import { getNewCustomer } from '../../../data/customers/customers.data.js';
+import { IOrder } from '../../../ui/types/order.types.js';
+import helpers from '../../../utils/helpers.js';
 
-describe('Smoke test on orders page', () => {
-  let productsArr = ['test 1', 'test 2', 'test 3'];
-  // TODO add to before hook products(4) and customers(2) creation
+describe('Create order tests', () => {
+  let orderId: string, order: IOrder;
+  let [product_01, product_02]: IProductResponse[] = [];
+  let [customer]: ICustomerResponse[] = [];
+
   before(async () => {
+    [product_01, product_02] = await ApiProductsActions.createProducts(2);
+    [customer] = await ApiCustomersActions.createCustomers(1);
+
     await SignInActions.openSalesPortal();
     await SignInActions.signIn();
     await HomeActions.openOrdersPage();
@@ -18,7 +31,25 @@ describe('Smoke test on orders page', () => {
   after(async () => {});
 
   it('Create order', async () => {
-    await OrderActions.createOrder('Anatoly', productsArr);
+    order = await OrderActions.createOrder(customer.name, [product_01.name, product_02.name]);
+    orderId = order._id;
+
     await OrdersAssertions.verifyToastMessage(orderPageToastMessages.orderCreated());
+    await OrdersAssertions.verifyCreatedOrderInTableRow(order);
   });
+
+  it('Should verify order data on "Order details" page', async () => {
+    // verify products in section
+    // total price
+    // status
+    // order history
+    await OrderActions.clickOnDetailsActionButton(orderId);
+    // await OrdersAssertions.verifyCustomerInCustomerDetailsSection(customer);
+    await OrdersDetailsActions.clickOnAllAccordionButtonsInProductDetailsSection();
+    await helpers.browserPause(5000);
+  });
+
+  // TODO
+
+  //
 });
--- src/ui/actions/orders/orders.actions.ts
+++ src/ui/actions/orders/orders.actions.ts
@@ -1,6 +1,10 @@
+import OrdersController from '../../../api/controllers/orders.controller.js';
+import { reqAsLoggedUser } from '../../../api/request/request-as-logged-user.js';
 import { logAction } from '../../../utils/reporter/allure.reporter.js';
 import CreateOrderModalPage from '../../pages/aqa_project/modals/orders_modals/create-order.modal.page.js';
 import OrdersPage from '../../pages/aqa_project/orders/orders.page.js';
+import { IOrder, ORDER_STATUSES } from '../../types/order.types.js';
+import { IProduct } from '../../types/products.types.js';
 import { CommonActions } from '../common.actions.js';
 import CreateOrderModalActions from '../modals/orders_modals/create-order.modal.actions.js';
 
@@ -18,23 +22,31 @@ class OrdersActions extends CommonActions {
   @logAction('Create order')
   async createOrder(customerName: string, products: string[]) {
     await this.clickOnCreateOrderButton();
-    await this.chooseDropdownItem(
-      CreateOrderModalPage['Customer dropdown'],
-      CreateOrderModalPage['Dropdown option'](customerName),
-    );
+    await this.chooseDropdownItem(CreateOrderModalPage['Customer dropdown'], CreateOrderModalPage['Dropdown option'](customerName));
     await CreateOrderModalActions.addProductsToOrder(products);
     await CreateOrderModalActions.clickOnCreateButton();
+    const order: IOrder = await this.getFilteredOrder(customerName, products);
+    return order;
   }
 
   @logAction('Get created order table row')
-  async getCreatedProductRow(orderNum: string) {
+  async getCreatedOrderRowInTable(orderId: string) {
     return {
-      name: await OrdersPage.waitForElemAndGetText(OrdersPage['Name by table row'](orderNum)),
-      price: await OrdersPage.waitForElemAndGetText(OrdersPage['Price by table row'](orderNum)),
-      status: await OrdersPage.waitForElemAndGetText(OrdersPage['Status by table row'](orderNum)),
-      number: await OrdersPage.waitForElemAndGetText(OrdersPage['Order number by table row'](orderNum)),
-      delivery: await OrdersPage.waitForElemAndGetText(OrdersPage['Delivery by table row'](orderNum)),
+      name: await OrdersPage.waitForElemAndGetText(OrdersPage['Name by table row'](orderId)),
+      price: await OrdersPage.waitForElemAndGetText(OrdersPage['Price by table row'](orderId)),
+      status: await OrdersPage.waitForElemAndGetText(OrdersPage['Status by table row'](orderId)),
+      orderNumber: await OrdersPage.waitForElemAndGetText(OrdersPage['Order number by table row'](orderId)),
+      delivery: await OrdersPage.waitForElemAndGetText(OrdersPage['Delivery by table row'](orderId)),
     };
+  }
+
+  async getFilteredOrder(customerName: string, products: string[]) {
+    const order: IOrder = (await reqAsLoggedUser(OrdersController.getOrder, {})).data.Orders.find(
+      (o: IOrder) =>
+        o.customer.name === customerName &&
+        o.products.filter((p: IProduct) => products.includes(p.name) && o.status === ORDER_STATUSES.DRAFT),
+    );
+    return order;
   }
 }
 
