ecab8f49db1c7a183326f59d36549752c43be9fe
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2024-02-16 00:27:54 +0200

    refactor ElementFinder

--- src/utils/element-finder.ts
+++ src/utils/element-finder.ts
@@ -1,19 +1,21 @@
-const findElement = async (selector: string): Promise<WebdriverIO.Element> => {
-  try {
-    const element = await $(selector);
-    return element;
-  } catch (error: any) {
-    throw new Error(`Error while finding element by selector: ${selector}`);
+class ElementFinder {
+  async findElement(selector: string): Promise<WebdriverIO.Element> {
+    try {
+      const element = await $(selector);
+      return element;
+    } catch (error: any) {
+      throw new Error(`Error while finding element by selector: ${selector}`);
+    }
   }
-};
 
-const findArrayElements = async (selector: string): Promise<WebdriverIO.ElementArray> => {
-  try {
-    const elements = await $$(selector);
-    return elements;
-  } catch (error) {
-    throw new Error(`Error while finding array of elements by selector: ${selector}`);
+  async findArrayElements(selector: string): Promise<WebdriverIO.ElementArray> {
+    try {
+      const elements = await $$(selector);
+      return elements;
+    } catch (error) {
+      throw new Error(`Error while finding array of elements by selector: ${selector}`);
+    }
   }
-};
+}
 
-export const elementFinder = { findElement, findArrayElements };
+export default new ElementFinder();
